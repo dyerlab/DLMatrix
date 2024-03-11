8b4fed6bc2d953ca4221a607eedc331d6ff4c7bd
dyerlab <dyerlab@icloud.com>
2024-03-11 13:35:51 -0400

    forcing upcate

--- Sources/DLMatrix/Extensions/Array+Vector.swift
+++ Sources/DLMatrix/Extensions/Array+Vector.swift
@@ -1,6 +1,13 @@
 //
-//  File.swift
-//  
+//  dyerlab.org                                          @dyerlab
+//                      _                 _       _
+//                   __| |_   _  ___ _ __| | __ _| |__
+//                  / _` | | | |/ _ \ '__| |/ _` | '_ \
+//                 | (_| | |_| |  __/ |  | | (_| | |_) |
+//                  \__,_|\__, |\___|_|  |_|\__,_|_.__/
+//                        |_ _/
+//
+//         Making Population Genetic Software That Doesn't Suck
 //
 //  Created by Rodney Dyer on 11/2/22.
 //
